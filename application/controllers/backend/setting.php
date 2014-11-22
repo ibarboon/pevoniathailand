1688e83623db2edece278ee5a18dbbd987a05ac3
ibarboon-imac <Wiboons@Wiboons-iMac.local>
2014-11-22 22:19:44 +0700

    Backend - Init. Setting View

--- application/controllers/backend/setting.php
+++ application/controllers/backend/setting.php
@@ -15,7 +15,7 @@ class Setting extends CI_Controller {
 		$params['current_page'] = ($this->uri->segment(2))? $this->uri->segment(2): 'dashboard';
 		$params['menu_list'] = $this->utility_model->get_option_by_type('backend_menu');
 		$this->load->view('backend/header_view', $params);
-		$this->load->view('backend/dashboard_view', $params);
+		$this->load->view('backend/setting_view', $params);
 		$this->load->view('backend/footer_view', $params);
 	}
 }
